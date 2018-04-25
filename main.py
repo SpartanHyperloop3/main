7f24670c4ffefcbf72e13ef97c43a17e0cca2771
Michael von Pohle <mvonpohle@gmail.com>
2018-04-25 00:17:08 -0700

    Created configuration skeleton, working on adding configuration, import, and function/class definitions

---
+++ main.py
@@ -0,0 +1,18 @@
+# ------------- import section -------------
+
+import RPi.GPIO as GPIO
+
+# ------------- config/definitions -------------
+
+# --- GPIO ---
+# Using GPIO.BOARD numbering. This is the numbering on the headers and follows this: https://pinout.xyz/
+# Info here: https://learn.sparkfun.com/tutorials/raspberry-gpio/python-rpigpio-api
+# (PIN_NUMBER, INPUT/OUTPUT, PULL_UP/PULL_DOWN)
+# Example: GPIO_CONFIG = ((12, GPIO.IN, GPIO.PUD_DOWN), (13, GPIO.OUT. GPIO.PUD_UP))
+
+
+# ------------- function/class definitions  -------------
+
+#
+
+# ------------- main code body -------------
