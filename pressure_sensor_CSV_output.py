12a43292b6eb53d7624e38239ea53d3bcc1c881f
Michael von Pohle <mvonpohle@gmail.com>
2018-07-06 01:23:09 -0700

    Read pressure sensor and output to CSV reading implemented, now just need to implement outputting to CSV

---
+++ pressure_sensor_CSV_output.py
@@ -0,0 +1,41 @@
+import smbus
+import time
+import numpy as np
+
+class ADC(smbus):
+
+    def __init__(self, device_address, read_register):
+        smbus.__init__(self, 1)
+        self.pec = True
+        self.deviceAddress = device_address
+        self.readRegister = read_register
+
+    def read_i2c_block_data(self):
+        super(ADC, self).read_i2c_block_data(self.deviceAddress, self.readRegister)
+        time.sleep(0.001)
+        return super(ADC, self).read_i2c_block_data(self.deviceAddress, self.readRegister)
+
+    def ADCToVolts12Bit(self):
+        reading = self.read_i2c_block_data()
+        upper = (reading[0] & 15)
+        upper = (upper << 8)
+        total = upper + reading[1]
+        voltage = float(total * .001)
+        return voltage
+
+class PressureSensor():
+
+    def __init__(self, resistor_value, current_interpolation_list, pressure_interpolation_list,
+                 device_address, read_register):
+        self._voltageInterpolationList = [x * resistor_value for x in current_interpolation_list]
+        self._pressureInterpolationList = pressure_interpolation_list
+        self.ADC = ADC(device_address, read_register)
+
+    def read(self):
+        voltage = ADC.ADCToVolts12Bit()
+        return np.interp(voltage, self._voltageInterpolationList, self._pressureInterpolationList)
+
+pressure_sensor = PressureSensor(resistor_value=200, current_interpolation_list=[4.014, 15, 20],
+                                 pressure_interpolation_list=[0.01, 200, 500],
+                                 device_address=105, read_register=48)
+print(pressure_sensor.read())
